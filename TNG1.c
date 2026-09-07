f403aa7ad83527d3fd60448b8e6722678e118247
tanushkaa12g <tanushkaa.12g@gmail.com>
2026-09-07 15:53:05 +0530

    Add files via upload

---
+++ TNG1.c
@@ -0,0 +1,24 @@
+/*Program (1) -> Write a program to make use of basic Input/Output functions using different data types.
+
+Solution(1) -> Predefined ValueS
+*/
+
+#include <stdio.h> 
+int main()
+{
+int rollnum;
+float per;
+char grade;
+
+rollnum = 55;
+per = 88.88;
+grade = 'A';
+
+printf("-----Student Information-----\n");
+printf("\n Roll Number: %d", rollnum);
+printf("\n Percentage: %f", per);
+printf("\n Grade : %c", grade);
+
+return 0;
+
+}
